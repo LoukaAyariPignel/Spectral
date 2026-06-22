54645ea694ee724aef9f4a85dc307c5567757329
Louka Ayari-Pignel <lou.ayaripignel@gmail.com>
2026-06-22 02:22:43 +0200

    fix: use DeferredRegister.Items + registerItem() so NeoForge injects item ID

--- src/main/java/fr/skylined/spectral/item/ModItems.java
+++ src/main/java/fr/skylined/spectral/item/ModItems.java
@@ -2,15 +2,11 @@ package fr.skylined.spectral.item;
 
 import fr.skylined.spectral.Spectral;
 import fr.skylined.spectral.item.custom.GemItem;
-import net.minecraft.core.registries.Registries;
-import net.minecraft.world.item.Item;
-import net.neoforged.neoforge.registries.DeferredHolder;
+import net.neoforged.neoforge.registries.DeferredItem;
 import net.neoforged.neoforge.registries.DeferredRegister;
 
 public class ModItems {
-    public static final DeferredRegister<Item> ITEMS =
-            DeferredRegister.create(Registries.ITEM, Spectral.MOD_ID);
+    public static final DeferredRegister.Items ITEMS = DeferredRegister.createItems(Spectral.MOD_ID);
 
-    public static final DeferredHolder<Item, GemItem> GEM =
-            ITEMS.register("gem", () -> new GemItem(new Item.Properties()));
+    public static final DeferredItem<GemItem> GEM = ITEMS.registerItem("gem", GemItem::new);
 }
