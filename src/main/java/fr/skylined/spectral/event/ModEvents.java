2f6e54406d288e0b2d2f840f3deed47d92e93ba0
Louka Ayari-Pignel <lou.ayaripignel@gmail.com>
2026-06-22 02:19:17 +0200

    fix NeoForge 26.1 API: new tint system, tooltip, onCraftedBy signatures

--- src/main/java/fr/skylined/spectral/event/ModEvents.java
+++ src/main/java/fr/skylined/spectral/event/ModEvents.java
@@ -7,13 +7,13 @@ import net.neoforged.bus.api.SubscribeEvent;
 import net.neoforged.fml.common.EventBusSubscriber;
 import net.neoforged.neoforge.event.BuildCreativeModeTabContentsEvent;
 
-@EventBusSubscriber(modid = Spectral.MOD_ID, bus = EventBusSubscriber.Bus.MOD)
+@EventBusSubscriber(modid = Spectral.MOD_ID)
 public class ModEvents {
 
     @SubscribeEvent
     public static void onBuildContents(BuildCreativeModeTabContentsEvent event) {
         if (event.getTabKey() == CreativeModeTabs.INGREDIENTS) {
-            event.accept(ModItems.GEM);
+            event.accept(ModItems.GEM.get());
         }
     }
 }
